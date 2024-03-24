aaf6779103a970f907c408d645675710a1242ddb
PedroPortow <pedrolportow@gmail.com>
2024-03-24 20:33:02 -0300

    readme e inicio do addtwonumberss

---
+++ addTwoNumbers/addTwoNumbers.py
@@ -0,0 +1,24 @@
+
+
+
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+		
+        
+        
